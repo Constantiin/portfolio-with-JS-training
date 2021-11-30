b83a166a457229c81212c993570af56ea4f8a091
Cos <constantiin@bk.ru>
2021-11-30 16:23:53 +0300

    add gallery

--- js/index.js
+++ js/index.js
@@ -103,3 +103,46 @@ const enabledScroll = () => {
 
     handlerBurger(selectorContacts, contactsMenuBtn, 'header__contacts_open');
 }
+
+{ // галерея
+
+    const portfolioList = document.querySelector('.portfolio__list');
+    const pageOverlay = document.createElement('div');
+    pageOverlay.classList.add('page__overlay');
+
+    portfolioList.addEventListener('click', (event) => {
+        const card = event.target.closest('.card');
+
+        if (card) {
+            document.body.append(pageOverlay);
+
+            disabledScroll();
+
+            const title = card.querySelector('.card__client');
+
+            const picture = document.createElement('picture');
+            picture.style.cssText = `
+                position: absolute;
+                top: 20px;
+                left: 50%;
+                transform: translateX(-50%);
+                width: 90%;
+                max-width: 1440px;
+            `;
+
+            picture.innerHTML = `
+                <source srcset="${card.dataset.fullImage}.avif" type="image/avif">
+                <source srcset="${card.dataset.fullImage}.webp" type="image/webp">
+                <img src="${card.dataset.fullImage}.jpg" alt="${title.textContent}">
+            `;
+
+            pageOverlay.append(picture);
+        };
+    });
+
+    pageOverlay.addEventListener('click', () => {
+        pageOverlay.textContent = '';
+        pageOverlay.remove();
+        enabledScroll();
+    });
+}
